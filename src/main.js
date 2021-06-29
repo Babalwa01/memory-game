8fd9a6f3b2ca14072d1e705abc586b81e9e0345b
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-30 01:24:52 +0200

    game made responsive

--- src/main.js
+++ src/main.js
@@ -5,23 +5,15 @@ const cards = document.querySelectorAll(".cell");
 const messageContainer = document.querySelector(".message-container");
 const playButton = document.querySelector("#play-button");
 const flipCount = document.querySelector("#flip-count");
-let board = document.querySelector("#board");
+let board = document.querySelector(".board");
+let twoByTwoBoard = document.querySelector(".two-by-two-board");
+let threeByTwoBoard = document.querySelector(".three-by-two-board");
+let fourByTwoBoard = document.querySelector(".four-by-two-board");
+let fourByThreeBoard = document.querySelector(".four-by-three-board");
 const fourByThreeBtn = document.querySelector("#four-by-three-btn");
 const fourByTwoBtn = document.querySelector("#four-by-two-btn");
 const threeByTwoBtn = document.querySelector("#three-by-two-btn");
 const twoByTwoBtn = document.querySelector("#two-by-two-btn");
-const jsDiv1 = document.querySelector(".js-div1");
-const jsDiv2 = document.querySelector(".js-div2");
-const cssDiv1 = document.querySelector(".css-div1");
-const cssDiv2 = document.querySelector(".css-div2");
-const linuxDiv1 = document.querySelector(".linux-div1");
-const linuxDiv2 = document.querySelector(".linux-div2");
-const reactDiv1 = document.querySelector(".react-div1");
-const reactDiv2 = document.querySelector(".react-div2");
-const githubDiv1 = document.querySelector(".github-div1");
-const githubDiv2 = document.querySelector(".github-div2");
-const htmlDiv1 = document.querySelector(".html-div1");
-const htmlDiv2 = document.querySelector(".html-div2");
 const boardSizesDiv = document.querySelector("#board-sizes-div");
 const timer = document.querySelector("#timer");
 let hr = 0;
@@ -179,7 +171,7 @@ function showMessage() {
 }
 
 function showFourByThreeBoard() {
-    board.style.display = "grid";
+    fourByThreeBoard.style.display = "grid";
     boardSizesDiv.style.display = "none";
     headerP.innerHTML = "Click a tile then match it";
     board.style.paddingLeft = "30%";
@@ -187,43 +179,10 @@ function showFourByThreeBoard() {
     timerHeading.style.visibility = "visible";
 }
 
-function hideHTMLDivs() {
-    htmlDiv1.style.display = "none";
-    htmlDiv2.style.display = "none";
-}
-
-function hideJSDivs() {
-    jsDiv1.style.display = "none";
-    jsDiv2.style.display = "none";
-}
-
-function hideCSSDivs() {
-    cssDiv1.style.display = "none";
-    cssDiv2.style.display = "none";
-}
-
-function hideLinuxDivs() {
-    linuxDiv1.style.display = "none";
-    linuxDiv2.style.display = "none";
-}
-
-function hideReactDivs() {
-    reactDiv1.style.display = "none";
-    reactDiv2.style.display = "none";
-}
-
-function hideGithubDivs() {
-    githubDiv1.style.display = "none";
-    githubDiv2.style.display = "none";
-}
-
 function showFourByTwoBoard() {
-    board.style.display = "grid";
+    fourByTwoBoard.style.display = "grid";
     boardSizesDiv.style.display = "none";
-    showAllCards();
     fourByTwoClicked = true;
-    hideHTMLDivs();
-    hideJSDivs();
     board.style.gridTemplateColumns = "repeat(4, 30%)";
     boardSizesDiv.style.marginLeft = "9rem";
     messageContainer.style.left = "2rem";
@@ -235,13 +194,9 @@ function showFourByTwoBoard() {
 }
 
 function showThreeByTwoBoard() {
-    board.style.display = "grid";
+    threeByTwoBoard.style.display = "grid";
     boardSizesDiv.style.display = "none";
-    showAllCards();
     threeByTwoClicked = true;
-    hideHTMLDivs();
-    hideJSDivs();
-    hideCSSDivs();
     board.style.gridTemplateColumns = "repeat(3, 30%)";
     board.style.gridColumnGap = "0.9em";
     board.style.gridRowGap = "0.8em";
@@ -252,11 +207,9 @@ function showThreeByTwoBoard() {
 }
 
 function showTwoByTwoBoard() {
-    board.style.display = "grid";
+    twoByTwoBoard.style.display = "grid";
     boardSizesDiv.style.display = "none";
     twoByTwoClicked = true;
-    showThreeByTwoBoard();
-    hideReactDivs();
     board.style.gridTemplateColumns = "repeat(2, 50%)";
     headerP.innerHTML = "Click a tile then match it";
     board.style.gridColumnGap = "0.6em";
@@ -264,21 +217,6 @@ function showTwoByTwoBoard() {
     board.style.paddingLeft = "40%";
     board.style.paddingRight = "40%";
     timerHeading.style.visibility = "visible";
-}
-
-function showAllCards() {
-    jsDiv1.style.display = "flex";
-    jsDiv2.style.display = "flex";
-    cssDiv1.style.display = "flex";
-    cssDiv2.style.display = "flex";
-    linuxDiv1.style.display = "flex";
-    linuxDiv2.style.display = "flex";
-    reactDiv1.style.display = "flex";
-    reactDiv2.style.display = "flex";
-    githubDiv1.style.display = "flex";
-    githubDiv2.style.display = "flex";
-    htmlDiv1.style.display = "flex";
-    htmlDiv2.style.display = "flex";
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard));
